276a76294c3d0a7378708f2c605f048a096fa735
va2083 <va2083@c240g5-110215.wisc.cloudlab.us>
2021-09-24 11:56:18 -0500

    Update benchmark script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-models=( 2 8 9 10 9,10)
+models=( 2 8 9 10 8,9 8,10 9,10 )
 
-for filename in ./media; do
-    for model in models; do
-        echo "dotnet run --project src/VAP/VideoPipelineCore/ $filename sample.txt 1 1 $model car person motorbike"
-done
+set -o errexit
+
+for filename in ./media/*; do
+    name=${filename##*/}
+    for model in ${models[@]}; do
+        dotnet run --project src/VAP/VideoPipelineCore/ $name sample.txt 1 1 $model car person motorbike
+    done || exit 1
+done || exit 1
